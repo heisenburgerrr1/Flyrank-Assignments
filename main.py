0f47d5b63f1aa81c2543ffec0f9357940fd39204
heisenburgerrr1 <heisenberg10w@gmail.com>
2026-07-26 01:15:03 +0400

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -57,23 +57,23 @@ def create_task(new_task: TaskCreate):
     return task
 
 
-# @app.put("/tasks/{task_id}")
-# def update_task(task_id: int, updated: TaskCreate):
-#     if not updated.title or not updated.title.strip():
-#         raise HTTPException(status_code=400, detail="Title is required")
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated: TaskCreate):
+    if not updated.title or not updated.title.strip():
+        raise HTTPException(status_code=400, detail="Title is required")
 
-#     for task in tasks:
-#         if task.id == task_id:
-#             task.title = updated.title
-#             return task
+    for task in tasks:
+        if task.id == task_id:
+            task.title = updated.title
+            return task
 
-#     raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
 
 
-# @app.delete("/tasks/{task_id}", status_code=204)
-# def delete_task(task_id: int):
-#     for i, task in enumerate(tasks):
-#         if task.id == task_id:
-#             tasks.pop(i)
-#             return
-#     raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for i, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(i)
+            return
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
